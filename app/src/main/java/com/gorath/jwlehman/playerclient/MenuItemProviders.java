a2651f52cfe13d616218314fec44bdc6420c7c9c
Jeremy Lehman <jeremylehman9@gmail.com>
2015-10-11 12:49:13 -0400

    Added MenuItemProvider and added chat forum to it

---
+++ app/src/main/java/com/gorath/jwlehman/playerclient/MenuItemProviders.java
@@ -0,0 +1,15 @@
+package com.gorath.jwlehman.playerclient;
+
+import com.gorath.jwlehman.playerclient.Model.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jeremy on 10/11/2015.
+ */
+public class MenuItemProviders {
+    private List<MenuItem> items = new ArrayList<MenuItem>(){{
+        add(new MenuItem("Chat Forums"));
+    }};
+}
